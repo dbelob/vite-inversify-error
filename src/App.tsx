30153d93040578d99a419043be6de690ddc80239
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2024-10-24 19:52:25 +0300

    Add components by class and function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ClassComponent from './ClassComponent.tsx';
+import FuctionComponent from './FuctionComponent.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,6 +29,12 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
+      </p>
+      <p>
+          <ClassComponent/>
+      </p>
+        <p>
+          <FuctionComponent/>
       </p>
     </>
   )
